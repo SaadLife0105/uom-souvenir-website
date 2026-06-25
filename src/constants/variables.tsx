9ed6f48774f709e9dfa27933d4e7aed82883ee8d
SaadLife0105 <168929839+SaadLife0105@users.noreply.github.com>
2026-06-25 23:27:18 +0400

    initial variables file

---
+++ src/constants/variables.tsx
@@ -0,0 +1,6 @@
+export const background = "#8BDFF9";
+export const gold = "#FFCB70";
+export const creme = "#F8EEDE";
+export const red = "#E1423D";
+export const darkBlue = "#1A3C7D";
+export const black = "#1E2019";
